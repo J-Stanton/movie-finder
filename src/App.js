e8397a4483aa8daa5c5669a6a3f4a27fcbd3a14f
J-Stanton <jameskstanton123@gmail.com>
2024-08-07 21:36:21 +0200

    Fetch data and link relevant files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import {useEffect} from "react";
+import './App.css';
+import SearchIcon from './search.svg';
+const API_URL = 'http://www.omdbapi.com?apikey=45e71256';
+
 
 const App=() =>{
+
+    const searchMovies = async (title)=>{
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        
+    }
+    useEffect(()=>{
+        searchMovies('Avatar');
+    },[])
     return (
         <h1>App</h1>
     );
